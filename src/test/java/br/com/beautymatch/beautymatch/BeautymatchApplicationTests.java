9a3e3b9039314d5855fbcd1e95903642b1f8bba6
JoãoGuimarães <victor.joao1476@gmail.com>
2025-04-21 16:35:26 -0300

    Crud criado para a entidade/classe Profissional

---
+++ src/test/java/br/com/beautymatch/beautymatch/BeautymatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.beautymatch.beautymatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautymatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
