a2732a81d8cb134ea191be93f98b176a9b340b67
WesleyRodriguez <168235704+WesleyRodriguez@users.noreply.github.com>
2025-05-02 18:01:31 -0300

    getrs setrs e construtores das classes

--- src/main/java/com/beauty/com/MatchBeauty/entity/Profissional.java
+++ src/main/java/com/beauty/com/MatchBeauty/entity/Profissional.java
@@ -3,8 +3,13 @@ package com.beauty.com.MatchBeauty.entity;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
+import java.time.LocalDateTime;
+
 @Entity
 @DiscriminatorValue("PROFISSIONAL")
 public class Profissional extends Usuario {
+    public Profissional(Long idUsuario, String username, String password, String email, String telefone, LocalDateTime criadoEm, LocalDateTime atualizadoEm) {
+        super(idUsuario, username, password, email, telefone, criadoEm, atualizadoEm);
+    }
     // Pode adicionar métodos específicos de Profissional se necessário
 } 
--- src/main/java/com/beauty/com/MatchBeauty/entity/Proprietario.java
+++ src/main/java/com/beauty/com/MatchBeauty/entity/Proprietario.java
@@ -3,8 +3,13 @@ package com.beauty.com.MatchBeauty.entity;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
+import java.time.LocalDateTime;
+
 @Entity
 @DiscriminatorValue("PROPRIETARIO")
 public class Proprietario extends Usuario {
+    public Proprietario(Long idUsuario, String username, String password, String email, String telefone, LocalDateTime criadoEm, LocalDateTime atualizadoEm) {
+        super(idUsuario, username, password, email, telefone, criadoEm, atualizadoEm);
+    }
     // Pode adicionar métodos específicos de Proprietario se necessário
 } 
