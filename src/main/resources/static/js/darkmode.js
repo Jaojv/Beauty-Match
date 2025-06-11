fafec4d68ddf6f14f6794e1e91a7e3e400ddfe03
mateusgeraldoc <mateusgeraldocunha@gmail.com>
2025-06-11 17:04:07 -0300

    Adicionando elementos do menu inicial

--- src/main/resources/static/js/darkmode.js
+++ src/main/resources/static/js/darkmode.js
@@ -1,7 +1,18 @@
 const link = document.getElementById('darkmode-botao');
 const body = document.body;
 
+// Ao carregar a página, verifica o storage
+if (localStorage.getItem('dark-mode') === 'enabled') {
+  body.classList.add('dark-mode');
+}
+
 link.addEventListener('click', (event) => {
-  event.preventDefault(); // Impede o comportamento padrão de link (navegação)
+  event.preventDefault();
   body.classList.toggle('dark-mode');
+  // Salva a preferência
+  if (body.classList.contains('dark-mode')) {
+    localStorage.setItem('dark-mode', 'enabled');
+  } else {
+    localStorage.setItem('dark-mode', 'disabled');
+  }
 });
