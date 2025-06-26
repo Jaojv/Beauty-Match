103bc80cb08d2ba5818b397e90c74295eefe2282
JoãoGuimarães <victor.joao1476@gmail.com>
2025-06-26 10:41:44 -0300

    Corrigida a atribuição de roles no JWT conforme tipoUsuario

--- src/main/java/com/beauty/com/MatchBeauty/entity/Usuario.java
+++ src/main/java/com/beauty/com/MatchBeauty/entity/Usuario.java
@@ -156,7 +156,7 @@ public class Usuario implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.tipoUsuario.name()));
     }
 
     @Override
