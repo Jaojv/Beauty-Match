5f56f44c179ab46cd30573fb0a00615fb82badfb
JoãoGuimarães <victor.joao1476@gmail.com>
2025-07-11 19:49:21 -0300

    Deletando arquivos de testes

--- src/main/java/com/beauty/com/MatchBeauty/MatchBeautyApplication.java
+++ src/main/java/com/beauty/com/MatchBeauty/MatchBeautyApplication.java
@@ -1,29 +1,13 @@
 package com.beauty.com.MatchBeauty;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.beauty.com.MatchBeauty.service.AutenticacaoService;
-import com.beauty.com.MatchBeauty.service.MenuService;
 
 @SpringBootApplication
 public class MatchBeautyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MatchBeautyApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner init(AutenticacaoService autenticacaoService, MenuService menuService) {
-		return args -> {
-			while (true) {
-				if (autenticacaoService.realizarLogin()) {
-					menuService.exibirMenu();
-				}
-			}
-		};
 	}
 
 }
